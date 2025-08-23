3b9bd726aac7485910ac27b1938502ee5b71dc5f
Vishwas Singh <117565226+thevishwass@users.noreply.github.com>
2025-08-24 02:31:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const spotlight = document.querySelector('.spotlight');
-
-document.addEventListener('mousemove', (e) => {
-    spotlight.style.left = `${e.clientX}px`;
-    spotlight.style.top = `${e.clientY}px`;
-});
-
